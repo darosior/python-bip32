a92c77e9aee48fb2b7af2cad2429d686b28bc822
darosior <darosior@protonmail.com>
2020-02-12 00:22:46 +0100

    bip32: add a high-level class to derive keys

--- bip32/bip32.py
+++ bip32/bip32.py
@@ -140,3 +140,83 @@ def _unserialize_extended_key(extended_key):
     index = int.from_bytes(extended_key[9:14], "big")
     chaincode, key = extended_key[14:47], extended_key[47:]
     return prefix, depth, fingerprint, index, chaincode, key
+
+
+class BIP32:
+    def __init__(self, chaincode, privkey=None, pubkey=None, index=0):
+        """
+        :param chaincode: The master chaincode, used to derive keys. As bytes.
+        :param privkey: The master private key for this index (default 0).
+                        Can be None for pubkey-only derivation.
+                        As bytes.
+        :param pubkey: The master public key for this index (default 0).
+                       Can be None if private key is specified.
+                       Compressed format. As bytes.
+        :param index: The current index of the derivation. As int.
+        """
+        assert isinstance(chaincode, bytes)
+        assert privkey is not None or pubkey is not None
+        if privkey is not None:
+            assert isinstance(privkey, bytes)
+        if pubkey is not None:
+            assert isinstance(pubkey, bytes)
+        else:
+            pubkey = coincurve.PublicKey.from_secret(privkey).format()
+        self.master_chaincode = chaincode
+        self.master_privkey = privkey
+        self.master_pubkey = pubkey
+        self.master_index = index
+
+    def get_hardened_privkey(self, index, depth=0):
+        """Get the i-nth hardened private key of the given depth.
+
+        :param index: Which pubkey to derive. As int.
+        :param depth: The depth, also called "account", to derive the index-nth
+                      privkey from. As int.
+        :return privkey: The private key as bytes.
+        """
+        if self.master_privkey is None:
+            raise ValueError("Cannot derive a private key without the "
+                             "master key")
+        privkey, chaincode = self.master_privkey, self.master_chaincode
+        while depth > 0:
+            depth -= 1
+            # FIXME: suboptimal for mixed wallets..
+            privkey, chaincode = _derive_hardened_private_child(chaincode,
+                                                                privkey, 0)
+        privkey, _ = _derive_hardened_private_child(chaincode, privkey, index)
+        return privkey
+
+    def get_unhardened_privkey(self, index, depth=0):
+        """Get the i-nth *unhardened* private key of the given depth.
+
+        :param index: Which privkey to derive. As int.
+        :param depth: The depth (/account) to derive this index-nth private
+                      key from. As int.
+        :return privkey: The private key as bytes.
+        """
+        if self.master_privkey is None:
+            raise ValueError("Cannot derive a private key without the "
+                             "master key")
+        privkey, ccode = self.master_privkey, self.master_chaincode
+        while depth > 0:
+            depth -= 1
+            privkey, ccode = _derive_unhardened_private_child(ccode,
+                                                              privkey, 0)
+        privkey, _ = _derive_unhardened_private_child(ccode, privkey, index)
+        return privkey
+
+    def get_pubkey(self, index, depth=0):
+        """Get the i-nth public key of the given depth.
+
+        :param index: Which pubkey to derive. As int.
+        :param depth: The depth (/account) to derive this index-nth private
+                      key from. As int.
+        :return: The pubkey as bytes.
+        """
+        pubkey, chaincode = self.master_pubkey, self.master_chaincode
+        while depth > 0:
+            depth -= 1
+            pubkey, chaincode = _derive_public_child(pubkey, chaincode, 0)
+        pubkey, _ = _derive_public_child(pubkey, chaincode, index)
+        return pubkey
