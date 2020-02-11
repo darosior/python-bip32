1e7d98a4f65e99d9ec65681f51e46dd5ddb86b4e
darosior <darosior@protonmail.com>
2020-02-11 23:04:07 +0100

    bip32: add serialization functions

--- bip32/bip32.py
+++ bip32/bip32.py
@@ -4,6 +4,16 @@ import hmac
 
 
 HARDENED_INDEX = 0x80000000
+ENCODING_PREFIX = {
+    "main": {
+        "private": 0x0488ADE4,
+        "public": 0x0488B21E,
+    },
+    "test": {
+        "private": 0x04358394,
+        "public": 0x043587CF,
+    },
+}
 
 
 class BIP32DerivationError(Exception):
@@ -78,3 +88,55 @@ def _derive_public_child(pubkey, chaincode, index):
     parent_pub = coincurve.PublicKey(int.from_bytes(pubkey, "big"))
     child_pub = tmp_pub.add(parent_pub)
     return bytes.fromhex(child_pub.to_hex), payload[32:]
+
+
+def _serialize_extended_key(key, depth, fingerprint, index, chaincode,
+                            network="main"):
+    """Serialize an extended private *OR* public key, as spec by bip-0032.
+
+    :param key: The public or private key to serialize. Note that if this is
+                a public key it MUST be compressed.
+    :param depth: 0x00 for master nodes, 0x01 for level-1 derived keys, etc..
+    :param fingerprint: The first four bytes of the parent's pubkey.
+                        0x00000000 if master.
+    :param index: The index of the key being serialized. 0x00000000 if master.
+    :param chaincode: The chain code (not the labs !!).
+
+    :return: The serialized extended key.
+    """
+    for param in {key, fingerprint, chaincode}:
+        assert isinstance(param, bytes)
+    for param in {depth, index}:
+        assert isinstance(param, int)
+    # A privkey or a compressed pubkey
+    assert len(key) in {32, 33}
+    if network not in {"main", "test"}:
+        raise ValueError("Unsupported network")
+    is_privkey = len(key) == 32
+    prefix = ENCODING_PREFIX[network]["private" if is_privkey else "public"]
+    extended = prefix.to_bytes(4, "big")
+    extended += depth.to_bytes(1, "big")
+    extended += fingerprint
+    extended += index.to_bytes(4, "big")
+    extended += chaincode
+    if is_privkey:
+        extended += b'\x00'
+    extended += key
+    return extended
+
+
+def _unserialize_extended_key(extended_key):
+    """Unserialize an extended private *OR* public key, as spec by bip-0032.
+
+    :param extended_key: The extended key to unserialize __as bytes__
+
+    :return: prefix (int), depth (int), fingerprint (bytes), index (int),
+             chaincode (bytes), key (bytes)
+    """
+    assert isinstance(extended_key, bytes) and len(extended_key) == 78
+    prefix = int.from_bytes(extended_key[:4], "big")
+    depth = extended_key[4]
+    fingerprint = extended_key[4:9]
+    index = int.from_bytes(extended_key[9:14], "big")
+    chaincode, key = extended_key[14:47], extended_key[47:]
+    return prefix, depth, fingerprint, index, chaincode, key
