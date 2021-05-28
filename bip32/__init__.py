fa7a4754c446f139aed86fffc3ddda81a145950b
Antoine Poinsot <darosior@protonmail.com>
2021-05-28 18:01:15 +0200

    bip32: major version bump
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- bip32/__init__.py
+++ bip32/__init__.py
@@ -1,7 +1,7 @@
 from .bip32 import BIP32, PrivateDerivationError, InvalidInputError
 from .utils import BIP32DerivationError, HARDENED_INDEX
 
-__version__ = "0.1"
+__version__ = "1.0"
 
 __all__ = [
     "BIP32",
