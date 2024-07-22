db8257de1216c3fb8304b2a4f421f82526f59ed6
Antoine Poinsot <darosior@protonmail.com>
2024-07-22 14:04:56 +0200

    Release version 4.0

--- bip32/__init__.py
+++ bip32/__init__.py
@@ -1,7 +1,7 @@
 from .bip32 import BIP32, PrivateDerivationError, InvalidInputError
 from .utils import BIP32DerivationError, HARDENED_INDEX
 
-__version__ = "3.4"
+__version__ = "4.0"
 
 __all__ = [
     "BIP32",
